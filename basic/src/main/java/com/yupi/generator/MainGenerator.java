13553a21cc88285a20fdffeee039d9088ce44804
ZXEZ <1779925700@qq.com>
2024-06-08 22:22:38 +0800

    0608 下载教程源码继续

--- basic/src/main/java/com/yupi/generator/MainGenerator.java
+++ basic/src/main/java/com/yupi/generator/MainGenerator.java
@@ -8,26 +8,49 @@ import java.io.IOException;
 
 public class MainGenerator {
     public static void doGenerate(Object model) throws TemplateException, IOException {
+        String inputRootPath="D:\\Dy\\yupi\\generator\\yupi-generator\\acm-template-pro";
+        String outputRootPath="D:\\Dy\\yupi\\generator\\yupi-generator";
 
-        String projectPath = System.getProperty("user.dir");
-        File parentFile = new File(projectPath).getParentFile();
-        String inputPath =new File(parentFile, "acm-template").getAbsolutePath();
-        String outputPath =projectPath;
-        System.out.println("inputPath:"+inputPath);
-        System.out.println("outputPath:"+outputPath);
-        StaticGenerator.copyFilesByByRecursive(inputPath, outputPath);
-        String inputDynamicFilePath =projectPath+File.separator+"src/main/resources/templates/MainTemplate.java.ftl";
-        String outputDynamicFilePath =outputPath+File.separator+"acm-template/src/main/java/com/yupi/acm/MainTemplate.java";
-        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+        String inputPath;
+        String outputPath;
+        inputPath=new File(inputRootPath,"src/main/java/com/yupi/acm/MainTemplate.java.ftl").getAbsolutePath();
+        outputPath=new File(outputRootPath,"acm-template/src/main/java/com/yupi/acm/MainTemplate.java").getAbsolutePath();
+        DynamicGenerator.doGenerate(inputPath, outputPath, model);
+
+        inputPath=new File(inputRootPath,".gitignore").getAbsolutePath();
+        outputPath=new File(outputRootPath,".gitignore").getAbsolutePath();
+        StaticGenerator.copyFilesByHutool(inputPath, outputPath);
+
+        inputPath=new File(inputRootPath,"README.md").getAbsolutePath();
+        outputPath=new File(outputRootPath,"README.md").getAbsolutePath();
+        StaticGenerator.copyFilesByHutool(inputPath, outputPath);
+
+
+//        String projectPath = System.getProperty("user.dir");
+//        File parentFile = new File(projectPath).getParentFile();
+//        String inputPath =new File(parentFile, "acm-template").getAbsolutePath();
+//        String outputPath =projectPath;
+//        System.out.println("inputPath:"+inputPath);
+//        System.out.println("outputPath:"+outputPath);
+//        StaticGenerator.copyFilesByByRecursive(inputPath, outputPath);
+//        String inputDynamicFilePath =projectPath+File.separator+"src/main/resources/templates/MainTemplate.java.ftl";
+//        String outputDynamicFilePath =outputPath+File.separator+"acm-template/src/main/java/com/yupi/acm/MainTemplate.java";
+//        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
 
 
     }
     public static void main(String[] args) throws TemplateException, IOException {
+
         MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
         mainTemplateConfig.setAuthor("ZXEZ");
         mainTemplateConfig.setLoop(false);
-        mainTemplateConfig.setOutputText("求和结果:22");
+        mainTemplateConfig.setOutputText("求和结果:11");
         doGenerate(mainTemplateConfig);
+//        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+//        mainTemplateConfig.setAuthor("ZXEZ");
+//        mainTemplateConfig.setLoop(false);
+//        mainTemplateConfig.setOutputText("求和结果:22");
+//        doGenerate(mainTemplateConfig);
 
     }
 }
